c62174b9fe11898bbd287460c613bdd289e7dab3
asraful <asrafulislamasf@gmail.com>
2025-08-27 20:53:36 +0600

    JavaScript and First Functionality Added

---
+++ JScript/script.js
@@ -0,0 +1,9 @@
+let count=0;
+const heartIcon=document.querySelectorAll('.heart');
+for(let icons of heartIcon){
+    icons.addEventListener('click',function(){
+        count++;
+        const heartCounter=document.getElementById('heart-count');
+        heartCounter.innerText=count;
+    })
+}
